fe1281fb85e071cc91e1f4376aa77470dae29dba
jaesungahn <43926790+voyagerahn@users.noreply.github.com>
2023-06-14 11:34:07 +0900

    Update WheelOfFortuneAfterRefactoring.cpp

--- WheelOfFortuneAfterRefactoring.cpp
+++ WheelOfFortuneAfterRefactoring.cpp
@@ -1,11 +1,13 @@
 #include <string>
 #include <vector>
+#define MAX_LINE_COUNT 5
+#define TRY_COUNT_LIMIT 26
 
 using namespace std;
 
 class Wheel {
 public:
-	int getTotalPrice(vector<string> strs, string userdata) {
+	int getTotalPrice(vector<string> answerData, string userdata) {
 		int result = 0;
 		int continueCount = 0;
 		int firstChance[MAX_LINE_COUNT] = { 0 };
@@ -14,32 +16,32 @@ public:
 		for (int i = 0; i < TRY_COUNT_LIMIT; i++) {
 			char tryChar = userdata[i];
 
-			if (getPassCount(strs, tryChar) == 0)
+			if (getPassCount(answerData, tryChar) == 0)
 			{
 				clearMetadata(continueCount, secondChance);
 				continue;
 			}
 			continueCount++;
 
-			result += get2000Dollar(strs, tryChar, secondChance);
-			result += get1000Dollar(strs, tryChar, firstChance, secondChance);
-			result += get100Dollar(continueCount, getPassCount(strs, tryChar));
+			result += get2000Dollar(answerData, tryChar, secondChance);
+			result += get1000Dollar(answerData, tryChar, firstChance, secondChance);
+			result += get100Dollar(continueCount, getPassCount(answerData, tryChar));
 
-			flipCard(strs, tryChar);
+			flipCard(answerData, tryChar);
 		}
 
 		return result;
 	}
 
 private:
-	int get2000Dollar(vector<string> strs, char tryChar, int secondChance[5]) {
+	int get2000Dollar(vector<string> answerChar, char tryChar, int secondChance[5]) {
 		int result = 0;
 
-		for (int y = 0; y < strs.size(); y++) {
+		for (int y = 0; y < answerChar.size(); y++) {
 			if (secondChance[y] == -1) continue;
 
-			for (int x = 0; x < strs[secondChance[y]].size(); x++) {
-				if (strs[secondChance[y]][x] == tryChar) {
+			for (int x = 0; x < answerChar[secondChance[y]].size(); x++) {
+				if (answerChar[secondChance[y]][x] == tryChar) {
 					result += 2000;
 					break;
 				}
@@ -49,11 +51,11 @@ private:
 		return result;
 	}
 
-	int get1000Dollar(vector<string> strs, char tryChar, int firstChance[5], int secondChance[5]) {
+	int get1000Dollar(vector<string> answerChar, char tryChar, int firstChance[5], int secondChance[5]) {
 		int resultPrice = 0;
-		for (int y = 0; y < strs.size(); y++) {
-			for (int x = 0; x < strs[y].size(); x++) {
-				if (strs[y][x] != tryChar) continue;
+		for (int y = 0; y < answerChar.size(); y++) {
+			for (int x = 0; x < answerChar[y].size(); x++) {
+				if (answerChar[y][x] != tryChar) continue;
 
 				if (firstChance[y] == 0 && x == 0) {
 					resultPrice += 1000;
@@ -68,23 +70,23 @@ private:
 		return resultPrice;
 	}
 
-	int getPassCount(vector<string> strs, char tryChar) {
+	int getPassCount(vector<string> answerChar, char tryChar) {
 		int result = 0;
 
-		for (int y = 0; y < strs.size(); y++) {
-			for (int x = 0; x < strs[y].size(); x++) {
-				if (strs[y][x] != tryChar) continue;
+		for (int y = 0; y < answerChar.size(); y++) {
+			for (int x = 0; x < answerChar[y].size(); x++) {
+				if (answerChar[y][x] != tryChar) continue;
 				result++;
 			}
 		}
 		return result;
 	}
 
-	void flipCard(vector<string> strs, char tryChar) {
-		for (int y = 0; y < strs.size(); y++) {
-			for (int x = 0; x < strs[y].size(); x++) {
-				if (strs[y][x] != tryChar) continue;
-				strs[y][x] = '_';
+	void flipCard(vector<string> answerChar, char tryChar) {
+		for (int y = 0; y < answerChar.size(); y++) {
+			for (int x = 0; x < answerChar[y].size(); x++) {
+				if (answerChar[y][x] != tryChar) continue;
+				answerChar[y][x] = '_';
 			}
 		}
 	}
@@ -98,6 +100,4 @@ private:
 		for (int i = 0; i < MAX_LINE_COUNT; i++) chance[i] = -1;
 	}
 
-	const int TRY_COUNT_LIMIT = 26;
-	const int MAX_LINE_COUNT = 5;
 };
